8ab19f6a7c304e1f108009ae573c101db7226b42
Kiran Ravula <kiranrsp@gmail.com>
2024-05-06 21:35:28 +1000

    fial commit

--- models/movie.js
+++ models/movie.js
@@ -12,7 +12,16 @@ const reviewSchema = new Schema({
     min: 1,
     max: 5,
     default: 5
-  }
+  },
+  // Don't forget to add the comma above then
+  // add the 3 new properties below
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  userName: String,
+  userAvatar: String
 }, {
   timestamps: true
 });
